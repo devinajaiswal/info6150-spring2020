43a19434d90448315036ff42f47e76b2f534fc61
SylvanW <wang.mengqi2@husky.neu.edu>
2020-02-15 13:46:35 -0500

    add modal in footjumbotron

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,58 +1,74 @@
 <template>
   <div id="Modal">
-    <b-button v-b-modal.modal-1>Show Group Members</b-button>
-    <b-modal id="modal-1" title="Group Member">
-      <b-table :fields="fields" :items="members">
-        <template v-slot:cell(badge)="data">
-          <!-- <b class="text-info">{{ data.item.text }}</b> -->
-          <b-badge v-bind:variant="data.item.type">{{
-            data.item.text
-          }}</b-badge>
-        </template>
-      </b-table>
+    <b-button v-b-modal.modal-1>Sign up</b-button>
+
+    <b-modal id="modal-1" centered title="Confirm your email">
+      <p class="my-4">An email has been sent to your email address.</p>
+      <p>Please verify it!</p>
     </b-modal>
   </div>
 </template>
 <script>
 export default {
-  name: "Modal",
-  data() {
-    return {
-      fields: ["nuid", "name", "badge"],
-      members: [
-        {
-          nuid: "1492338",
-          name: "Ziyu Zhou",
-          type: "danger",
-          text: "Danger"
-        },
-        {
-          nuid: "1400149",
-          name: "Vaibhavi Khamar",
-          type: "success",
-          text: "Danger"
-        },
-        {
-          nuid: "1472783",
-          name: "Mengqi Wang",
-          type: "success",
-          text: "Environmentally friendly"
-        },
-        {
-          nuid: "1306506",
-          name: "Devina Jaiswal",
-          type: "success",
-          text: "Danger"
-        }
-      ]
-    };
-  }
+  name: "Modal"
+  // data() {
+  //   return {
+  //     fields: ["nuid", "name", "badge"],
+  //     members: [
+  //       {
+  //         nuid: "1492338",
+  //         name: "Ziyu Zhou",
+  //         type: "danger",
+  //         text: "Danger"
+  //       },
+  //       {
+  //         nuid: "1400149",
+  //         name: "Vaibhavi Khamar",
+  //         type: "success",
+  //         text: "Danger"
+  //       },
+  //       {
+  //         nuid: "1472783",
+  //         name: "Mengqi Wang",
+  //         type: "success",
+  //         text: "Environmentally friendly"
+  //       },
+  //       {
+  //         nuid: "1306506",
+  //         name: "Devina Jaiswal",
+  //         type: "success",
+  //         text: "Danger"
+  //       }
+  //     ]
+  //   };
+  // }
 };
 </script>
 <style>
-#Modal {
-  margin-top: 20px;
-  margin-bottom: 20px;
+#Modal button {
+  color: #05668d !important;
+  background-color: #f0f3bd !important;
+  border: none !important;
 }
 
+#modal-1___BV_modal_header_ {
+  color: #f0f3bd;
+  background-color: #028090;
+}
+
+#modal-1___BV_modal_footer_ {
+  color: #f0f3bd;
+  background-color: #05668d;
+}
+
+#modal-1___BV_modal_content_ .btn-primary {
+  color: #05668d;
+  background-color: #f0f3bd;
+  border: none !important;
+}
+#modal-1___BV_modal_content_ .btn-secondary {
+  color: #05668d !important;
+  background-color: #f0f3bd !important;
+  border: none !important;
+}
 </style>
