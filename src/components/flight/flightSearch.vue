ba23905015c334dede6f79137084b3afde9f518e
devina8 <jaiswal.de@husky.neu.edu>
2020-04-17 00:52:48 -0400

    Flight Search

---
+++ src/components/flight/flightSearch.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="mainContainer">
+    <div class="mainHeader "><span>Traveling the globe making dreams come true!</span></div>
+    <div class="contentContainer"> 
+   <el-card>
+       
+   <el-form >
+       <div>
+        <el-radio-group v-model="radio">
+  <el-radio :label="1">One Way</el-radio>
+  <el-radio :label="2">Round trip</el-radio>
+  
+</el-radio-group>
+       </div>
+       <div>
+<el-input
+  placeholder="From City"
+  v-model="fromCity"
+  clearable>
+</el-input>
+       </div>
+       <div>
+<el-input
+  placeholder="To City"
+  v-model="toCity"
+  clearable>
+</el-input>
+       </div>
+       <div>
+      <el-date-picker
+      v-model="departureDate"
+      type="date"
+      placeholder="Departure"
+      format="MM/dd/yyyy"
+      value-format="MM-dd-yyyy">
+    </el-date-picker>
+       </div>
+
+          <div>
+      <el-date-picker
+      v-model="returnDate"
+      type="date"
+      placeholder="Return"
+      format="yyyy/MM/dd"
+      value-format="yyyy-MM-dd">
+    </el-date-picker>
+       </div>
+    </el-form>
+    
+
+
+  </el-card>
+    </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    
+    data() {
+      return {
+         radio: 1,
+         fromCity: '',
+         toCity: '',
+        departureDate: '',
+        returnDate: '',
+        }
+      
+    
+  }
+  }
+</script>
+
+<style scoped>
+.mainHeader{
+   color: darkcyan;
+   font-size: 30px;
+   text-align: center;
+   font-weight: bold;
+   font-family: Arial, Helvetica, sans-serif;
+   
+}
+
+
+
+
+
+</style>
--- src/views/Flights.vue
+++ src/views/Flights.vue
@@ -1,56 +1,16 @@
 <template>
-  <div class="flights">
-    <b-row>
-      <b-col>
-        Airline
-      </b-col>
-      <b-col>
-        Flight Number
-      </b-col>
-      <b-col>
-        Price
-      </b-col>
-    </b-row>
-    <b-container v-for="(items, index) in json" :key="index">
-      {{ index }}
-      <b-row v-for="(item, index) in items" :key="index">
-        <b-col>
-          {{ item.airline }}
-        </b-col>
-        <b-col>
-          {{ item.flight_number }}
-        </b-col>
-        <b-col>
-          {{ item.price }}
-        </b-col>
-      </b-row>
-    </b-container>
-  </div>
+  <div class="Flights">
+   <flightSearch></flightSearch> 
+   </div>
 </template>
+
 <script>
+// @ is an alias to /src
+
+import flightSearch from "@/components/flight/flightSearch.vue";
 export default {
-  data() {
-    return {
-      json: {}
-    };
-  },
-  mounted() {
-    this.getJson();
-  },
-  methods: {
-    getJson() {
-      this.$axiosUtil
-        .get("api/v1/prices/cheap", {
-          origin: "BOS",
-          token: "ee5e3a59b78d121e99fd83e4420d6ccd",
-          page: "1",
-          currency: "USD"
-        })
-        .then(response => {
-          console.log(response.data.data);
-          this.json = response.data.data;
-        });
-    }
+  components: {
+  flightSearch
   }
 };
 </script>
